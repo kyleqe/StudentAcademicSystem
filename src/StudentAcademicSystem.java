e1ed0cd2d4d1e253d1f70ca90a7f5c9738abb8e6
Kyle Hove <kylehove222@gmail.com>
2026-08-19 03:30:10 -0700

    Added getting student info

--- src/StudentAcademicSystem.java
+++ src/StudentAcademicSystem.java
@@ -1,4 +1,22 @@
 import java.util.Scanner,
 public class StudentAcademicSystem {
     
+    // student fields
+    String name;
+    int age;
+    double mark;
+    double attendence;
+    String category;
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        StudentAcademicSystem student = new StudentAcademicSystem();
+        
+        //getting student infor
+        System.out.println(" STUDENT ACADEMIC SYSTEM");
+        System.out.println("ENTER STUDENT NAME: ");
+        student.name = sc.nextLine();
+    }
 }
+
+
