f31181c472cc1d812fc77e143bdf0e806963226c
Kyle Hove <kylehove222@gmail.com>
2026-08-19 04:50:59 -0700

    Updated getting student info

--- src/StudentAcademicSystem.java
+++ src/StudentAcademicSystem.java
@@ -1,27 +1,19 @@
 import java.util.Scanner,
 public class StudentAcademicSystem {
     
-    // student fields
-    String name;
-    int age;
-    double mark;
-    double attendence;
-    String category;
-    
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        StudentAcademicSystem student = new StudentAcademicSystem();
-        
+    
         //getting student infor
         
         System.out.print("ENTER STUDENT NAME: ");
-        student.name = input.nextLine();
+        String name = input.nextLine();
         System.out.print("ENTER STUDENT AGE: ");
-        student.age = input.nextInt();
+        int age = input.nextInt();
         System.out.print("ENTER STUDENT MARK (0-100): ");
-        student.mark = input.nextDouble();
+        double mark = input.nextDouble();
         System.out.print("ENTER STUDENT ATTENDENCE (%): ");
-        student.attendence = input.nextDouble();
+        double attendence = input.nextDouble();
       
     }
 }
